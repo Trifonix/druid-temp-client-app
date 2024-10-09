1fbf652dbbcd312cb2c37d4ca945dd272af6846e
dmtwebdev <smithjerod@protonmail.com>
2024-10-09 15:48:48 +0300

    upd app name

--- apollo.config.js
+++ apollo.config.js
@@ -1,12 +1,9 @@
-// apollo.config.js
 module.exports = {
     client: {
         service: {
-            name: "vue-apollo-quasar-login",
-            // URL to the GraphQL API
+            name: "druid-temp-client-app",
             url: "https://app.stud.druid.1t.ru/graphql",
         },
-        // Files processed by the extension
         includes: ["src/**/*.vue", "src/**/*.js"],
     },
 };
