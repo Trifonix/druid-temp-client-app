a0e3e44cf05d5677bbb1a5ea6500769849d195f9
dmtwebdev <smithjerod@protonmail.com>
2024-10-17 12:32:53 +0300

    change MainPage and add PageLayout

---
+++ src/components/PageLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="page-layout">
+    <NavigationTree />
+
+    <div class="content q-pa-md">
+      <slot></slot>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import NavigationTree from "./NavigationTree.vue";
+</script>
+
+<style scoped>
+.page-layout {
+  display: flex;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+}
+</style>
